bfe9ec6976c908558f9982ad84a1b4f8b9976d69
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-06-29 20:49:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/TheBlackXYZ/155")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002101130967'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001870015374'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5821871362').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001828551401').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
